df50a0822f25de88a7e6e6002f990fc66433c5fd
Farkas Péter <farkas.peter1995@gmail.com>
2016-04-02 18:12:32 +0200

    Add flag on right click, minor changes

--- mine.js
+++ mine.js
@@ -8,19 +8,28 @@ function createField(selector) {
     for (i = 0; i < 10; i++) {
         var $tr = $('<tr>');
         for (j = 0; j < 10; j++) {
-			var rand = Math.floor(Math.random() * (5 - 1)) + 1;
+            var rand = Math.floor(Math.random() * (5 - 1)) + 1;
             $tr.append(
-                $('<td>').click(function() {
-                    if (firstClick) {
-                        firstClick = false;
-                        generateMines($(this));
-                        generateNumbers();
+                $('<td>').mousedown(function (e) {
+                    switch (e.which) {
+                        case 1:
+                            if (firstClick) {
+                                firstClick = false;
+                                generateMines($(this));
+                                generateNumbers();
+                            }
+
+                            if (!over) {
+                                showNumbers($(this));
+                            }
+                            break;
+                        case 3:
+                        default:
+                            placeFlag($(this));
+                            break;
                     }
-					
-					if(!over){
-						showNumbers($(this));
-					}
-					
+                }).on("contextmenu", function () {
+                    return false;
                 }).attr('y', j).attr('x', i).attr('n', 0).attr('bg', rand));
         }
         $table.append($tr);
@@ -33,80 +42,25 @@ function generateMines($clicked_td) {
     var y = $clicked_td.attr('y');
     var $tds = $('td[x!=' + x + '], td[y!=' + y + ']');
 
-    $tds.each(function() {
+    $tds.each(function () {
         if (Math.random() < 0.2) {
             $(this).addClass('c4');
         }
     });
 }
 
-function placeFlag($clicked_td) {
-	$clicked_td.addClass('flag');
-	
-	if($('td.c4, :not(td.flag)').size() == 0){
-		gameWon();
-	}
-}
-
-
-function showAllMines() {
-	$('.c4').each(function(){
-		$(this).addClass('visible');
-	});
-}
-
-function gameOver($clicked_td){
-	$clicked_td.removeClass('c4');
-	$clicked_td.addClass('explode');
-	showAllMines();
-	alert('faggit');
-	over = true;
-}
-
-function gameWon(){
-	
-}
-
-function showNumbers($clicked_td) {
-    var done = false;
-	var index = $clicked_td.index();
-	
-	if($clicked_td.attr('n') == 0 && !$clicked_td.hasClass('grass')){
-		$clicked_td.addClass('grass');
-		findGrass($clicked_td);
-	}
-	else if(parseInt($clicked_td.attr('n')) > 0 && !$clicked_td.hasClass('c4')){
-		$clicked_td.addClass('visible');
-	}
-	else if($clicked_td.hasClass('c4')){
-		gameOver($clicked_td);
-	}
-}
-
-function findGrass($current){
-	var x = parseInt($current.attr('x'));
-    var y = parseInt($current.attr('y'));
-
-    for (var i = x-1; i <= x+1; i++) {
-        for (var j = y-1; j <= y+1; j++) {
-            var $this = $('td[x=\"' + i + '\"][y=\"' + j + '\"]');
-			showNumbers($this);
-        }
-    }
-}
-
 function generateNumbers() {
     var $mines = $('.c4');
 
-    console.log('#mines: ' + $mines.size());
+    //console.log('#mines: ' + $mines.size());
 
-    $mines.each(function() {
+    $mines.each(function () {
         var x = parseInt($(this).attr('x'));
         var y = parseInt($(this).attr('y'));
         var n;
 
-        for (var i = x-1; i <= x+1; i++) {
-            for (var j = y-1; j <= y+1; j++) {
+        for (var i = x - 1; i <= x + 1; i++) {
+            for (var j = y - 1; j <= y + 1; j++) {
                 var $this = $('td[x=\"' + i + '\"][y=\"' + j + '\"]');
                 n = parseInt($this.attr('n')) + 1;
                 $this.attr('n', n);
@@ -115,6 +69,61 @@ function generateNumbers() {
     });
 }
 
-$(document).ready(function() {
+function placeFlag($clicked_td) {
+    $clicked_td.addClass('flag');
+
+    if ($('td.c4, :not(td.flag)').size() == 0) {
+        gameWon();
+    }
+}
+
+function gameWon() {
+    alert('You won!');
+}
+
+function showNumbers($clicked_td) {
+    //var done = false;
+    //var index = $clicked_td.index();
+
+    if ($clicked_td.attr('n') == 0 && !$clicked_td.hasClass('grass')) {
+        $clicked_td.addClass('grass');
+        findGrass($clicked_td);
+    }
+    else if (parseInt($clicked_td.attr('n')) > 0 && !$clicked_td.hasClass('c4')) {
+        $clicked_td.addClass('visible');
+    }
+    else if ($clicked_td.hasClass('c4')) {
+        gameOver($clicked_td);
+    }
+}
+
+function findGrass($current) {
+    var x = parseInt($current.attr('x'));
+    var y = parseInt($current.attr('y'));
+
+    for (var i = x - 1; i <= x + 1; i++) {
+        for (var j = y - 1; j <= y + 1; j++) {
+            var $this = $('td[x=\"' + i + '\"][y=\"' + j + '\"]');
+            showNumbers($this);
+        }
+    }
+}
+
+function gameOver($clicked_td) {
+    $clicked_td.removeClass('c4');
+    $clicked_td.addClass('explode');
+    showAllMines();
+    alert('Game over!');
+    over = true;
+}
+
+function showAllMines() {
+    $('.c4').each(function () {
+        $(this).addClass('visible');
+    });
+}
+
+
+$(document).ready(function () {
     createField('#minefield');
 });
